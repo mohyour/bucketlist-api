90b7d17b4877058ad6b4d5c622f270c6be1f042a
mohyour <mohyour01@gmail.com>
2018-06-03 23:41:30 +0100

    create database model

---
+++ src/models.py
@@ -0,0 +1,35 @@
+from src import db
+
+
+class BucketList(db.Model):
+    """Creating the bucketlist table"""
+
+    __tablename__ = "bucketlists"
+
+    id = db.column(db.Integer, primary_key=True)
+    name = db.column(db.String(255))
+    date_created = db.column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.column(db.DateTime, default=db.func.current_timestamp(),
+    onupdate=db.func.current_timestamp())
+
+    def __init__(self, name):
+        """Initialize with name"""
+        self.name = name
+
+    def save(self):
+        """Save new entry to db"""
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        """delete entry from db"""
+        db.session.delete(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_all()
+        """Get all bucketlist entries"""
+        return BucketList.query.all()
+
+    def __repr__(self):
+        return "BucketList {0}".format(self.name)
